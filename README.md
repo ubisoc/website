c51ada07592a8bce10988913473b936634344521
oss6 <ossedb@gmail.com>
2016-12-29 18:45:04 +0000

    Added relative links

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# website
-University of Birmingham Islamic Society website
+# University of Birmingham Islamic Society website
+
+## Deploying
+
+Follow this: http://stackoverflow.com/questions/27680342/how-to-deploy-a-middleman-site-to-github-user-page
--- config.rb
+++ config.rb
@@ -15,6 +15,9 @@ page '/*.txt', layout: false
 set :css_dir, 'css'
 set :js_dir, 'js'
 
+activate :relative_assets
+set :relative_links, true
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
